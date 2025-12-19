cf715b5c5e8e3ceb3cba0b7ef5bd0022f2b663a7
William-AViana <willdev.viana@gmail.com>
2025-12-18 22:40:12 -0300

    create main file on call functions

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #importar os resultados de cada API
+from generate_clients import generate_clients_to_cvs
 
+
+users = generate_clients_to_cvs()
+
+
+
+print(users)
 # Criar um único data frame e enviar
