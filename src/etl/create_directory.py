6b5305c6adbee176d8c8710a8052927a2ab33aa1
William-AViana <willdev.viana@gmail.com>
2025-12-20 22:00:00 -0300

    function for create directory with file

---
+++ src/etl/create_directory.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+def create_dir_whith_file():
+    source = Path("raw_data")
+    source.mkdir(parents=True, exist_ok=True)
+    
+    folder_path = source / 'clients.csv'
+    return folder_path    
