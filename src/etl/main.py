235833e53836d13758203f5bc86ddb7c0eda86a7
William-AViana <willdev.viana@gmail.com>
2025-12-21 20:05:52 -0300

    file for call load data

--- src/etl/main.py
+++ src/etl/main.py
@@ -1,12 +1,5 @@
-#importar os resultados de cada API
-from generate_clients import generate_clients_to_cvs
-import create_directory as cd
+import sys
+from load_data import load_data
 
-# all in function
-
-users = generate_clients_to_cvs()
-
-
-
-print(users)
-# Criar um único data frame e enviar
+if __name__ == "__main__":
+    sys.exit(load_data())
