8c05192d8ab8dd80b85e2beda65a0af6383a8586
William-AViana <willdev.viana@gmail.com>
2025-12-21 20:01:23 -0300

    update name function

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from create_directory import create_dir_whith_file
+from create_directory import create_dir_whith_file_csv
 
 
 def read_file_and_return_users():
-    file_path = create_dir_whith_file()
+    file_path = create_dir_whith_file_csv()
     
     users = pd.read_csv(file_path).to_dict(orient='records')
 
