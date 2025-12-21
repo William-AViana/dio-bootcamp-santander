6fb30f4e6f97ae2fa9e0df84276b114e04fe5adc
William-AViana <willdev.viana@gmail.com>
2025-12-21 20:03:46 -0300

    update scripts for function

--- src/etl/get_criptos.py
+++ src/etl/get_criptos.py
@@ -3,23 +3,20 @@ import locale
 from datetime import datetime
 import pandas as pd
 
-URL = 'https://api.coinbase.com/v2/prices/BTC-BRL/spot'
+def get_bitcoin():
+    URL = 'https://api.coinbase.com/v2/prices/BTC-BRL/spot'
 
-response = requests.get(URL)
+    response = requests.get(URL)
 
-data = response.json()
+    data = response.json()
 
-#transform data on object and extract informations
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-result_price = float(data['data']['amount'])
-df = pd.DataFrame([{
-    'Ativo': data['data']['base'],
-    'Moeda': data['data']['currency'],
-    'Preço': locale.currency(result_price, grouping=True),
-    'Data': datetime.now().strftime("%d-%M-%Y %H:%M")
-    }])
-
-
-print(df)
-
-#create file or send informationsfrom database
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+    result_price = float(data['data']['amount'])
+    df = pd.DataFrame([{
+        'ativo': data['data']['base'],
+        'moeda': data['data']['currency'],
+        'preco': locale.currency(result_price, grouping=True),
+        'data': datetime.now().strftime("%d-%M-%Y %H:%M")
+        }])
+    
+    return df['preco'][0]
